95ec88adc0c2a11bb338b19d1ca59a374b3782fe
Greg Skluzacek <gskluzacek@gmail.com>
2026-01-30 15:54:01 -0600

    added one more test similar to test_line_init_none_already_set in test_line.py
    instead of using an invalid line (its neither horizontal or verticaql) we create a horizontal line and set its line_orientation to Vertical.

--- tests/test_line.py
+++ tests/test_line.py
@@ -19,6 +19,8 @@ def test_line_init_vertical():
     assert line.orientation == LineOrientation.VERT
 
 def test_line_init_none_already_set():
+    # TODO: this test kind of exposes a flaw in the implementation, a line that is not horizontal or vertical can
+    #  be created and the edit bypassed by manually setting the p2's line_orientation to something not equal to NONE
     p1 = Point(0, 0)
     p2 = Point(10, 10) # Not horizontal or vertical
     p2.line_orientation = LineOrientation.HORZ
@@ -30,6 +32,19 @@ def test_line_init_none_already_set():
     line = Line(p1, p2)
     
     assert line.orientation == LineOrientation.HORZ
+
+def test_line_init_none_already_set_incorrect():
+    p1 = Point(0, 0)
+    p2 = Point(10, 0) # p1 & p2 form a horizontal line
+    p2.line_orientation = LineOrientation.VERT
+
+    # we, on purpose, manually set the line orientation to the incorrect value of vertical for a horizontal line
+    # if set_line_orientation is triggered when creating the line (and it should not be since line_orientation
+    # does not equal NONE), it will update the orientation to horizontal.
+    line = Line(p1, p2)
+
+    # validate that the orientation is still (incorrectly set to) vertical
+    assert line.orientation == LineOrientation.VERT
 
 def test_line_orientation_error():
     p1 = Point(0, 0)
